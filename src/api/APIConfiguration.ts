a3cf69aae083cb2d1ce497ca4c89e1a4e38bc775
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2023-12-09 19:37:42 +0100

    make api configuration configurable

--- src/api/APIConfiguration.ts
+++ src/api/APIConfiguration.ts
@@ -1,4 +1,23 @@
-export const APIConfiguration = {
-    apiGwUrl: `${window.location.protocol}//api.${window.location.host}/api/v1`,
-    apiCdnUrl: `${window.location.protocol}//cdn.${window.location.host}`
+interface APIConfig {
+    /**
+     * The API gateway URL.
+     */
+    apiGwUrl: string;
+
+    /**
+     * The CDN url.
+     */
+    apiCdnUrl: string;
 }
+
+let config : APIConfig = {} as APIConfig;
+updateApiConfig(window.location.protocol, window.location.host);
+
+export function updateApiConfig(protocol: string, host: string) {
+    config = {
+        apiGwUrl: `${protocol}//api.${host}/api/v1`,
+        apiCdnUrl: `${protocol}//cdn.${host}`
+    };
+}
+
+export const APIConfiguration = config;
