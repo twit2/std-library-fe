aa23920cbd951c80f37e1fffe4cbbc6dbf462388
GustavoFringGaming <113339801+GustavoFringGaming@users.noreply.github.com>
2024-01-12 07:33:26 -0500

    add ws url

--- src/api/APIConfiguration.ts
+++ src/api/APIConfiguration.ts
@@ -8,6 +8,11 @@ interface APIConfig {
      * The CDN url.
      */
     apiCdnUrl: string;
+
+    /**
+     * Websocket url.
+     */
+    apiWsUrl: string;
 }
 
 let config : APIConfig = {} as APIConfig;
@@ -16,6 +21,7 @@ updateApiConfig(window.location.protocol, window.location.host);
 export function updateApiConfig(protocol: string, host: string) {
     config.apiGwUrl = `${protocol}//api.${host}/api/v1`;
     config.apiCdnUrl = `${protocol}//cdn.${host}`;
+    config.apiWsUrl = `${protocol}//ws.${host}`;
 }
 
 export const APIConfiguration = config;
